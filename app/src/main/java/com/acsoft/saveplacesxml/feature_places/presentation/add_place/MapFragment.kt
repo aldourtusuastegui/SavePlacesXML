0e818567bd6c5db4d0d204699b918068b10ce1b1
aldourtusuastegui <aldo.urtu@gmail.com>
2022-09-27 21:37:12 -0600

    Se comparte ubicacion con intente, google maps

--- app/src/main/java/com/acsoft/saveplacesxml/feature_places/presentation/add_place/MapFragment.kt
+++ app/src/main/java/com/acsoft/saveplacesxml/feature_places/presentation/add_place/MapFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.acsoft.saveplacesxml.R
 import com.acsoft.saveplacesxml.databinding.FragmentMapBinding
 import com.acsoft.saveplacesxml.feature_places.domain.model.Place
@@ -71,6 +72,7 @@ class MapFragment : Fragment(), EasyPermissions.PermissionCallbacks {
 
         binding.buttonContinue.setOnClickListener {
             if (validateForm()) {
+                binding.buttonContinue.isClickable = false
                 val place = Place(
                     title = binding.etTitle.text.toString(),
                     description = binding.etDescription.text.toString(),
@@ -78,6 +80,8 @@ class MapFragment : Fragment(), EasyPermissions.PermissionCallbacks {
                     longitude = longitude
                 )
                 addPlaceViewModel.registerPlace(place)
+                findNavController().navigate(R.id.action_mapFragment_to_FirstFragment)
+                Toast.makeText(requireContext(),getString(R.string.place_save_success),Toast.LENGTH_LONG).show()
             } else {
                 Toast.makeText(requireContext(),getString(R.string.complete_form_to_continue),Toast.LENGTH_SHORT).show()
             }
--- app/src/main/java/com/acsoft/saveplacesxml/feature_places/presentation/places/PlacesListAdapter.kt
+++ app/src/main/java/com/acsoft/saveplacesxml/feature_places/presentation/places/PlacesListAdapter.kt
@@ -1,9 +1,13 @@
 package com.acsoft.saveplacesxml.feature_places.presentation.places
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.acsoft.saveplacesxml.R
 import com.acsoft.saveplacesxml.core.BaseViewHolder
 import com.acsoft.saveplacesxml.databinding.PlaceItemLayoutBinding
 import com.acsoft.saveplacesxml.feature_places.domain.model.Place
@@ -39,6 +43,27 @@ class PlacesListAdapter() : RecyclerView.Adapter<BaseViewHolder<*>>()  {
         override fun bind(item: Place) {
             binding.tvTitle.text = item.title
             binding.tvDescription.text = item.description
+            binding.ivShare.setOnClickListener {
+                shareLocation(context,item)
+            }
         }
+    }
+
+    private fun shareDataIntent(context: Context, place: Place) {
+        val intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TITLE,"Titulo: ${place.title} ")
+            putExtra(Intent.EXTRA_TEXT,"Descripción: ${place.description} con coordenadas: ${place.latitude} / ${place.longitude}")
+            type = "text/plain"
+        }
+        val shareIntent = Intent.createChooser(intent,"Save Places")
+        startActivity(context,shareIntent,null)
+    }
+
+    private fun shareLocation(context: Context, place: Place) {
+        val gmmIntentUri = Uri.parse("geo:${place.latitude},${place.longitude}")
+        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+        mapIntent.setPackage("com.google.android.apps.maps")
+        startActivity(context,mapIntent,null)
     }
 }
