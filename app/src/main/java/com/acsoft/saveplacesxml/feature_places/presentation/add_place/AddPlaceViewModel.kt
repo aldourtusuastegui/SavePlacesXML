92e3838cebf7b0b6ecfbba98305718ed48b2a813
aldourtusuastegui <13209412+aldourtusuastegui@users.noreply.github.com>
2022-10-29 23:18:38 -0600

    se crearon deeplinks para home y para mapfragment

--- app/src/main/java/com/acsoft/saveplacesxml/feature_places/presentation/add_place/AddPlaceViewModel.kt
+++ app/src/main/java/com/acsoft/saveplacesxml/feature_places/presentation/add_place/AddPlaceViewModel.kt
@@ -1,5 +1,6 @@
 package com.acsoft.saveplacesxml.feature_places.presentation.add_place
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.acsoft.saveplacesxml.feature_places.domain.model.Place
@@ -9,11 +10,23 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class AddPlaceViewModel @Inject constructor(private val placeUseCases: PlaceUseCases) : ViewModel() {
+class AddPlaceViewModel @Inject constructor(private val placeUseCases: PlaceUseCases,
+private val savedStateHandle: SavedStateHandle) : ViewModel() {
 
     fun registerPlace(place: Place) {
         viewModelScope.launch {
             placeUseCases.insertPlaceUseCase.invoke(place)
         }
     }
+
+    fun getTitleArg() : String {
+        val title = savedStateHandle.get<String>("title")
+        return if (title.isNullOrEmpty()) "" else title
+    }
+
+    fun getDescriptionArg() : String {
+        val description = savedStateHandle.get<String>("description")
+        return if (description.isNullOrEmpty()) "" else description
+    }
+
 }
